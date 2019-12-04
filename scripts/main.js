2e63025aa5fa212ff3108620dbe4f289e05afab5
Stegura <danylostegyra@gmail.com>
2019-12-04 10:40:45 +0200

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const blockBtns = document.querySelectorAll('[data-block-btn]')
+
+function hideCover() {
+    let element = this;
+    let coverElement = element.parentElement.querySelector('[data-cover]');
+
+    element.classList.toggle('active');
+    coverElement.classList.toggle('hidden')
+}
+
+blockBtns.forEach( e => e.onclick = hideCover);
+const openModalBtn = document.getElementById('open-modal');
+const closeModalBtn = document.getElementById('close-modal');
+const modalWindow = document.getElementById('modal-form');
+
+openModalBtn.onclick = () =>  modalWindow.style.display = "block";
+
+closeModalBtn.onclick = () => modalWindow.style.display = "none";
